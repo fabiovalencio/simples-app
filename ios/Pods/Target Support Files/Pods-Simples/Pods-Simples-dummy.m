fbc25ebc0d7b3a70d67ef01eb2be92ed62f5f783
Fabio Valencio <fabiovalencio@hotmail.com>
2020-09-16 14:45:07 -0300

    add android build

--- ios/Pods/Target Support Files/Pods-Simples/Pods-Simples-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Simples : NSObject
-@end
-@implementation PodsDummy_Pods_Simples
-@end
--- ios/Pods/Target Support Files/Pods-SimplesTests/Pods-SimplesTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SimplesTests : NSObject
-@end
-@implementation PodsDummy_Pods_SimplesTests
-@end
